4fdd91e02ef6758031f98b18a427fdeccf1b48e5
volosgoto <volosgoto@yahoo.com>
2018-10-01 15:11:42 +0300

    add basic html structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ class App extends Component {
   
   render(){
     return(
-      <div>Music master from App</div>
+      <div>
+        <div className="App-title">
+          Music master from App
+          </div>
+        <div>
+          <input placeholder="search for artist..."/>
+          <button>Search</button>
+        </div>
+        <div className="Profile"> 
+          <div>Artist name</div>
+          <div>Artist picture</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
     );
   }
 }
