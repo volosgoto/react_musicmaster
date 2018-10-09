034a33f476e3d257cd693c6443a19ff0989e3508
volosgoto <volosgoto@yahoo.com>
2018-10-09 14:44:14 +0300

    add track props

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,9 +4,41 @@ import './App.css';
 class Gallery extends Component {
   render() {
     console.log('gallery props', this.props);
+    // let tracks = this.props.tracks;
+    let { tracks }= this.props;
+
 
     return (
-      <div>Gallery</div>
+      <div>
+        { tracks.map( (track, k) => {
+          let trackImg = '';
+          return (
+          <div>
+            
+            <div
+            key = {k}
+            className = "track"
+          >
+            <img 
+              src={trackImg} 
+              className = "track-img" 
+              alt="track"
+
+            />
+          </div>
+          <p className = "track-text">
+            {/* {track.name} */}
+          </p>
+          
+          </div>
+          
+
+          ); 
+        } ) 
+
+        }
+
+      </div>
     )
   }
 }
